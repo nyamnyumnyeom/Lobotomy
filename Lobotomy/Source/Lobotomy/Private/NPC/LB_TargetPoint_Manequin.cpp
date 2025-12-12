8da46c2d3a8aaf268bfe5bc95bf504b0a1bb3f90
Potato_Nim <alswo0103kk@naver.com>
2025-12-12 10:44:11 +0900

    [ Add ] 마네킹 타겟포인트 추가

---
+++ Lobotomy/Source/Lobotomy/Private/NPC/LB_TargetPoint_Manequin.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NPC/LB_TargetPoint_Manequin.h"
+
+ALB_TargetPoint_Manequin::ALB_TargetPoint_Manequin()
+{
+
+}
