014ee152e605d9b3a4a9fab7b5e95a05bb6c4a83
nyamnyumnyeom <jincar000@naver.com>
2026-04-08 17:16:46 +0900

    [ Fetch ]
    - [x]  침대 카메라 오른쪽으로
    - in , out , failed 전부 오른쪽 전용으로 제작
    - is right 분기점 추가해서 right 인스턴스에서 설정시 작동
    - [x]  drop item 수정하기
    - 소리 기능 추가 ( 컴포넌트 힛 )
    - 물리 계산 추가 ( 충돌 힘 등 계산해서 소리 크기 및 피치 선형 계산 )
    - Drop point 추가 ( 오버 랩 되면 0~5초 뒤에 물리 계산 키고 force 주기 )
    - [x]  곰돌이 초기 배치 + 츠쿠요미방 배치

--- Lobotomy/Source/Lobotomy/Private/ActorBase/LB_DroppropsBase.cpp
+++ Lobotomy/Source/Lobotomy/Private/ActorBase/LB_DroppropsBase.cpp
@@ -1,6 +1,8 @@
 #include "ActorBase/LB_DroppropsBase.h"
 #include "Components/StaticMeshComponent.h"
 #include "Components/ArrowComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/AudioComponent.h"
 
 ALB_DroppropsBase::ALB_DroppropsBase()
 {
@@ -13,22 +15,111 @@ ALB_DroppropsBase::ALB_DroppropsBase()
     DropDirectionArrow->SetupAttachment(BaseMesh);
 
     BaseMesh->SetSimulatePhysics(false);
+    BaseMesh->SetNotifyRigidBodyCollision(true);
+}
+
+void ALB_DroppropsBase::BeginPlay()
+{
+    Super::BeginPlay();
+
+    GetComponents<UStaticMeshComponent>(CachedMeshComponents);
+
+    for (UStaticMeshComponent* MeshComp : CachedMeshComponents)
+    {
+        if (!MeshComp) continue;
+
+        MeshComp->SetNotifyRigidBodyCollision(true);
+        MeshComp->OnComponentHit.AddDynamic(this, &ALB_DroppropsBase::OnMeshHit);
+
+        InitialMeshWorldTransforms.Add(MeshComp, MeshComp->GetComponentTransform());
+    }
 }
 
 void ALB_DroppropsBase::Dropforce()
 {
     const FVector DropDirection = DropDirectionArrow->GetForwardVector();
+    const FVector Impulse = DropDirection * DropForceMagnitude;
 
-    TArray<UStaticMeshComponent*> MeshComponents;
-    GetComponents<UStaticMeshComponent>(MeshComponents);
-
-    for (UStaticMeshComponent* MeshComp : MeshComponents)
+    for (UStaticMeshComponent* MeshComp : CachedMeshComponents)
     {
         if (!MeshComp) continue;
 
         MeshComp->SetSimulatePhysics(true);
-
-        const FVector Impulse = DropDirection * DropForceMagnitude;
+        MeshComp->SetNotifyRigidBodyCollision(true);
         MeshComp->AddImpulse(Impulse, NAME_None, true);
+    }
+}
+
+void ALB_DroppropsBase::ResetDropProps()
+{
+    for (UStaticMeshComponent* MeshComp : CachedMeshComponents)
+    {
+        if (!MeshComp) continue;
+
+        MeshComp->SetPhysicsLinearVelocity(FVector::ZeroVector);
+        MeshComp->SetPhysicsAngularVelocityInDegrees(FVector::ZeroVector);
+        MeshComp->SetSimulatePhysics(false);
+
+        if (const FTransform* InitialTransform = InitialMeshWorldTransforms.Find(MeshComp))
+        {
+            MeshComp->SetWorldTransform(*InitialTransform, false, nullptr, ETeleportType::TeleportPhysics);
+        }
+    }
+
+    LastImpactSoundTime = -1000.0f;
+}
+
+void ALB_DroppropsBase::OnMeshHit(
+    UPrimitiveComponent* HitComponent,
+    AActor* OtherActor,
+    UPrimitiveComponent* OtherComp,
+    FVector NormalImpulse,
+    const FHitResult& Hit)
+{
+    if (!DropImpactSound) return;
+    if (!HitComponent) return;
+    if (OtherActor == this) return;
+    if (!GetWorld()) return;
+
+    const float ImpactStrength = NormalImpulse.Size();
+    if (ImpactStrength < MinImpactStrengthForSound) return;
+
+    const float CurrentTime = GetWorld()->GetTimeSeconds();
+    if (CurrentTime - LastImpactSoundTime < ImpactSoundCooldown) return;
+
+    LastImpactSoundTime = CurrentTime;
+
+    const float ClampedImpact = FMath::Clamp(
+        ImpactStrength,
+        MinImpactStrengthForSound,
+        MaxImpactStrengthForSound
+    );
+
+    const float NormalizedImpact = FMath::GetRangePct(
+        MinImpactStrengthForSound,
+        MaxImpactStrengthForSound,
+        ClampedImpact
+    );
+
+    const float VolumeMultiplier = FMath::Lerp(
+        MinVolumeMultiplier,
+        MaxVolumeMultiplier,
+        NormalizedImpact
+    );
+
+    const float PitchMultiplier = FMath::Lerp(
+        MinPitchMultiplier,
+        MaxPitchMultiplier,
+        NormalizedImpact
+    );
+
+    if (UAudioComponent* AudioComp = UGameplayStatics::SpawnSoundAtLocation(
+        this,
+        DropImpactSound,
+        Hit.ImpactPoint
+    ))
+    {
+        AudioComp->SetVolumeMultiplier(VolumeMultiplier);
+        AudioComp->SetPitchMultiplier(PitchMultiplier);
     }
 }
