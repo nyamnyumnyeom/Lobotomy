6c390b438cb3b2b85d134ead9863f6b0db996fe3
potato-bback <192658740+potato-Unreal@users.noreply.github.com>
2026-06-22 15:27:01 +0900

    Stove Signature Verifier 주석처리

--- Lobotomy/Source/Lobotomy/Lobotomy.Build.cs
+++ Lobotomy/Source/Lobotomy/Lobotomy.Build.cs
@@ -57,6 +57,6 @@ public class Lobotomy : ModuleRules
         }
 
         // Stove Signature Verifier 보안 강화 정적 라이브러리 링크 추가
-        PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "StoveSignatureVerifier.lib"));
+        //PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "StoveSignatureVerifier.lib"));
     }
 }
